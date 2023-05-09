974c25dd73934b9c5239e47c4ab6d05f0b4ae87d
Kiyarash <kiarashnadri@yahoo.com>
2023-05-09 21:13:54 +0330

    work on Project component

--- src/Project.jsx
+++ src/Project.jsx
@@ -3,6 +3,7 @@ const Project = () => {
     <div className="title">
       <h2 className="project">Projects</h2>
       <div className="underline"></div>
+      <p>for add a slidet to websit</p>
     </div>
   );
 };
