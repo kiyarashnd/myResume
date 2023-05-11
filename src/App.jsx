54ea7d7c544e1ace627ef6dd7385e565b4da66c1
Kiyarash <kiarashnadri@yahoo.com>
2023-05-11 13:46:31 +0330

    deploy page on github

--- src/App.jsx
+++ src/App.jsx
@@ -16,3 +16,4 @@ function App() {
 }
 
 export default App;
+// Inspired by https://gatsby-strapi-portfolio-project.netlify.app/
--- src/data.js
+++ src/data.js
@@ -1,14 +1,20 @@
+import tour from "./tour.png";
+import blog from "./blog.png";
+import tdl from "./tdl1.png";
+import weather from "./weather.png";
+import logIn from "./logIn.png";
+
 const people = [
   {
     id: 1,
-    image: "/src/tour.png",
+    image: tour,
     name: "our-Tours",
     description: "tour project use react that save your tours in cart",
     link: "https://github.com/kiyarashnd/our-Tours",
   },
   {
     id: 2,
-    image: "/src/blog.png",
+    image: blog,
     name: "Blog",
     description:
       "a blog website with react that you can add and delete blog in json server",
@@ -16,14 +22,14 @@ const people = [
   },
   {
     id: 3,
-    image: "/src/tdl1.png",
+    image: tdl,
     name: " toDoList",
     description: "to do list with html css javaScript that have dark mode",
     link: "https://github.com/kiyarashnd/toDoList",
   },
   {
     id: 4,
-    image: "/src/weather.png",
+    image: weather,
     name: " Weather-app",
     description:
       "weather app website with html css and javaScipt that get coordinate of user and have auto compelete and find weather of any city ",
@@ -31,7 +37,7 @@ const people = [
   },
   {
     id: 5,
-    image: "/src/logIn.png",
+    image: logIn,
     name: "logIn-signUP-page",
     description: "log in/sign up page use pure html css",
     link: "https://github.com/kiyarashnd/logIn-signUP-page",
